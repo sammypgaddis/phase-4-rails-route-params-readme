eafc362e98d5ce14d01f1cc4335fe552dfced875
Samuel Gaddis <siddag14@live.com>
2022-12-15 12:59:54 -0500

    commit tree 3b3fb16a6cef8b4c1c85aa16556e77673fb05fac

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,12 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show 
+    cheese = Cheese.find_by( id: params[:id] ) #creating the variable to use in line 10. then taking the class of Cheese
+    render json: cheese
+    
+  end
+
+
+
 end
